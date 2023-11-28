416dd8460ee3a314b362d290fb1bf6fe3b7d8f64
dela16 <Denice.94@hotmail.com>
2023-11-28 18:27:20 +0100

    finally have the checkboxes on place.

--- ListOfTODOs/Properties/DAL/Repositories/Repository.cs
+++ ListOfTODOs/Properties/DAL/Repositories/Repository.cs
@@ -16,7 +16,12 @@ namespace ListOfTODOs.Properties.DAL.Repositories
 
 		public async Task<bool> CreateItem(ToDoItem item)
 		{
-			await _dbContext.Items.AddAsync(item); //Sätt i variabel senare så du kan kolla så den inte är null osv. 
+			var result = await _dbContext.Items.AddAsync(item);
+
+			if (result is null)
+				Results.BadRequest("You have to add an item.");
+			//TODO Vill vi att programmet ska krascha and throw exception? 
+
 			await _dbContext.SaveChangesAsync();
 			return true; 
 		}
@@ -24,6 +29,10 @@ namespace ListOfTODOs.Properties.DAL.Repositories
 		public async Task<ToDoItem> UpdateItemById(ToDoItem updatedItem, int id)
 		{
 			var existingItem = await _dbContext.Items.FindAsync(id);
+
+			if (existingItem.Id != id)
+				Results.BadRequest("Sorry, no item with that id.");
+			//TODO Vill vi att programmet ska krascha and throw exception? 
 
 			existingItem.Activity = updatedItem.Activity;
 
@@ -34,12 +43,25 @@ namespace ListOfTODOs.Properties.DAL.Repositories
 
 		public async Task<List<ToDoItem>> GetAllItems()
 		{
+			if (_dbContext.Items.Count() < 0)
+				Results.BadRequest("No items in list.");
+
 			return await _dbContext.Items.ToListAsync(); 
 		}
 
 		public async Task<bool> DeleteItemById(int id)
 		{
 			var itemToBeDeleted = await _dbContext.Items.FindAsync(id);
+
+			if (itemToBeDeleted is null)
+			{
+				Results.BadRequest("You have to chose an id");
+			}
+			else if (itemToBeDeleted.Id != id)
+			{
+				Results.BadRequest("No item has that id.");
+			}
+
 			_dbContext.Remove(itemToBeDeleted);
 			await _dbContext.SaveChangesAsync();
 			return true; 
